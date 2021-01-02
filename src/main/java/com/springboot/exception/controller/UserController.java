98121fedc558b3b5dadb687ca7e7b08ad3efa50d
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-01-02 17:29:44 +0200

    Adding UserController. closes #1

---
+++ src/main/java/com/springboot/exception/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.springboot.exception.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Mohammed Amr
+ * @created 02/01/2021 - 17:21
+ * @project spring-boot-exception
+ */
+
+@Controller
+public class UserController {
+
+    @RequestMapping(path = "/add")
+    public String add() {
+        int num = 10 / 0;
+        return "add";
+    }
+}
