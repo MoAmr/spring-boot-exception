b6d94a9fcc87fd844fc63ad440ac8a5490e864e3
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-01-03 21:10:37 +0200

    Using Spring boot Exception Handler. closes #3

--- src/main/java/com/springboot/exception/controller/UserController.java
+++ src/main/java/com/springboot/exception/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.springboot.exception.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Mohammed Amr
@@ -15,6 +17,14 @@ public class UserController {
     @RequestMapping(path = "/add")
     public String add() {
         int num = 10 / 0;
-        return "error";
+        return "error.bak";
+    }
+
+    @ExceptionHandler(value = {ArithmeticException.class})
+    public ModelAndView handlerArithmeticException(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("mathError");
+        return modelAndView;
     }
 }
