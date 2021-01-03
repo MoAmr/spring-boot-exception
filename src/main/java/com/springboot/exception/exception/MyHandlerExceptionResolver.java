97100773a9df38d551ea4db4a9099f0c6aa531c3
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-01-03 23:43:09 +0200

    Adding HandlerExceptionResolver. closes #10

---
+++ src/main/java/com/springboot/exception/exception/MyHandlerExceptionResolver.java
@@ -0,0 +1,30 @@
+package com.springboot.exception.exception;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Mohammed Amr
+ * @created 03/01/2021 - 23:32
+ * @project spring-boot-exception
+ */
+
+public class MyHandlerExceptionResolver implements HandlerExceptionResolver{
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest,
+                                         HttpServletResponse httpServletResponse, @Nullable Object o, Exception e) {
+        ModelAndView mv = new ModelAndView();
+        if (e instanceof ArithmeticException) {
+            mv.setViewName("mathError");
+        }
+        if (e instanceof NullPointerException) {
+            mv.setViewName("nullPointerError");
+        }
+        mv.addObject("exception", e.toString());
+        return mv;
+    }
+}
